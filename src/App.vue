f11dd3ed60f632990af1581162f65509208a43e6
Stan Nedev <stan@nedev.io>
2019-11-14 18:38:14 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <p>This component’s code is in {{ filename }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      filename: 'App.vue'
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+}
+</script>
+
+<style>
+
+</style>
