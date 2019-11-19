dd28f6982e53fb958c7803ffa3d61510b61d96ae
Stan Nedev <stan@nedev.io>
2019-11-19 10:36:43 +0200

    Application structure changes

---
+++ src/components/Content.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="pagecontent">
+    <p>This child components of Content.vue are:</p>
+    <ul>
+      <li v-for="child in childComponents">{{ child }}</li>
+    </ul>
+    {{ weather_data }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["weather_data"],
+  data () {
+    return {
+      childComponents: ['TempVarChart.vue', 'Highlights.vue']
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+
+  },
+}
+</script>
+
+<style>
+#pagecontent {
+  border: 1px solid black;
+  padding: 2px;
+}
+</style>
